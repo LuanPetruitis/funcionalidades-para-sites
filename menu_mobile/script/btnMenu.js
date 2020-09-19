15bc42303bd58257895e3b5055c9531035ef0773
Luan Petruitis <luanpetruitis@hotmail.com>
2020-09-19 11:01:10 -0300

    funcionalidade

---
+++ menu_mobile/script/btnMenu.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('.menu');
+
+
+function funMenuOpen() {
+    menu.classList.add('menu_open');
+}
+
+function funMenuClose() {
+    menu.classList.remove('menu_open');
+}
